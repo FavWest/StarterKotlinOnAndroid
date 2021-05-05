d6dd12c7f474ffcf209507fb7f7ce998dfdf2b7f
Tori FavWest <practiceandthenpractice@gmail.com>
2021-05-05 09:32:02 -0700

    initial commit

---
+++ app/src/main/java/com/favwest/starterkotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.favwest.starterkotlin
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.webkit.WebView
+
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
